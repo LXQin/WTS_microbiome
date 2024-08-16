059391393907951b5602ed7a113762d7f258da1f
YSPeng <154644354+YSPeng1225@users.noreply.github.com>
2024-08-17 05:47:27 +0800

    Update hg38.sh

--- FASTQ_Processing/hg38.sh
+++ FASTQ_Processing/hg38.sh
@@ -1,6 +1,29 @@
-cd ~/microbiome/hg38/
-bwa mem -t 8 ./index/hg38.fa ../data/SRR8314071_1.fastq ../data/SRR8314071_2.fastq > ./sam/SRR8314071.sam
-samtools view -@ 8 -Sb ./sam/SRR8314071.sam | samtools sort -@ 8 -o ./bam/SRR8314071_sorted.bam
-samtools index -@ 8 ./bam/SRR8314071_sorted.bam ./bam/SRR8314071_sorted.bam.bai
-samtools view -b -f 4 ./bam/SRR8314071_sorted.bam > ./bam/SRR8314071_unmapped.bam
-bedtools bamtofastq -i ./bam/SRR8314071_unmapped.bam -fq ./processed_data/SRR8314071_unmapped_1.fastq -fq2 ./processed_data/SRR8314071_unmapped_2.fastq
+# Define variables
+input_dir="/home/nfs/pengy3/microbiome/data"
+trimmed_dir="/home/nfs/pengy3/microbiome/trimmed_files"
+hg38_index="/home/nfs/pengy3/microbiome/hg38/index/hg38.fa"
+bam_dir="/home/nfs/pengy3/microbiome/hg38/bam"
+unmapped_dir="/home/nfs/pengy3/microbiome/hg38/unmapped_files"
+sample_id="SRR8314071"
+
+# Trim Galore for paired-end reads
+trim_galore --paired "${input_dir}/${sample_id}_1.fastq" "${input_dir}/${sample_id}_2.fastq" -o "$trimmed_dir"
+
+# Alignment with BWA and sorting with samtools
+bwa mem -t 8 "$hg38_index" "${trimmed_dir}/${sample_id}_1_val_1.fq" "${trimmed_dir}/${sample_id}_2_val_2.fq" | \
+samtools sort -@ 8 -O bam -o "${bam_dir}/${sample_id}_sorted.bam"
+
+# Index the sorted BAM file
+samtools index -@ 8 "${bam_dir}/${sample_id}_sorted.bam"
+
+# Filter out mitochondrial reads (chrM) and save the result
+samtools view -h "${bam_dir}/${sample_id}_sorted.bam" | grep -v "chrM" | \
+samtools view -b -o "${bam_dir}/${sample_id}_no_mit.bam"
+
+# Extract unmapped reads
+samtools view -b -f 4 "${bam_dir}/${sample_id}_no_mit.bam" > "${bam_dir}/${sample_id}_unmapped.bam"
+
+# Convert BAM to FASTQ
+bedtools bamtofastq -i "${bam_dir}/${sample_id}_unmapped.bam" \
+-fq "${unmapped_dir}/${sample_id}_unmapped_1.fastq" \
+-fq2 "${unmapped_dir}/${sample_id}_unmapped_2.fastq"
