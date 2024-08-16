4f0488bbeab40417101199e64f29030a2f46a71d
YSPeng <154644354+YSPeng1225@users.noreply.github.com>
2024-08-17 06:16:24 +0800

    Update CHM13.sh

--- FASTQ_Processing/CHM13.sh
+++ FASTQ_Processing/CHM13.sh
@@ -1,13 +1,13 @@
 # Define variables
 base_dir="home/nfs/pengy3/microbiome/CHM13"
-index_dir="home/nfs/pengy3/microbiome/CHM13/index/chm13v2.0"
+chm13_dir="home/nfs/pengy3/microbiome/CHM13/index/chm13v2.0"
 input_dir="home/nfs/pengy3/microbiome/hg38/unmapped_files"
 bam_dir="home/nfs/pengy3/microbiome/CHM13/bam"
 unmapped_dir="home/nfs/pengy3/microbiome/CHM13/unmapped_files"
 sample_id="SRR8314071"
 
 # Alignment with Bowtie2 and sorting with Samtools
-bowtie2 -x "$index_dir" -1 "${input_dir}/${sample_id}_unmapped_1.fastq" -2 "${input_dir}/${sample_id}_unmapped_2.fastq" -p 8 | \
+bowtie2 -x "$chm13_dir" -1 "${input_dir}/${sample_id}_unmapped_1.fastq" -2 "${input_dir}/${sample_id}_unmapped_2.fastq" -p 8 | \
 samtools sort -O bam -o "${bam_dir}/${sample_id}_CHM13_sorted.bam"
 
 # Index the sorted BAM file
