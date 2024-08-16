922d62be1d8640e5a2b0c6cb18cea82f51d5ece0
YSPeng <154644354+YSPeng1225@users.noreply.github.com>
2024-08-17 05:46:52 +0800

    Update CHM13.sh

--- FASTQ_Processing/CHM13.sh
+++ FASTQ_Processing/CHM13.sh
@@ -1,6 +1,25 @@
-cd ~/microbiome/CHM13/
-bowtie2 -x ./index/chm13v2.0 -1 ../hg38/processed_data/SRR8314071_unmapped_1.fastq -2 ../hg38/processed_data/SRR8314071_unmapped_2.fastq -S ./sam/SRR8314071_CHM13.sam -p 8
-samtools view -Sb ./sam/SRR8314071_CHM13.sam | samtools sort -o ./bam/SRR8314071_CHM13_sorted.bam
-samtools index ./bam/SRR8314071_CHM13_sorted.bam ./bam/SRR8314071_CHM13_sorted.bam.bai
-samtools view -b -f 4 ./bam/SRR8314071_CHM13_sorted.bam > ./bam/SRR8314071_CHM13_unmapped.bam
-bedtools bamtofastq -i ./bam/SRR8314071_CHM13_unmapped.bam -fq ./processed_data/SRR8314071_final_unmapped_1.fastq -fq2 ./processed_data/SRR8314071_final_unmapped_2.fastq
+# Define variables
+base_dir=~/microbiome/CHM13
+index_dir="$base_dir/index/chm13v2.0"
+input_dir=~/microbiome/hg38/unmapped_files
+bam_dir="$base_dir/bam"
+unmapped_dir="$base_dir/unmapped_files"
+sample_id="SRR8314071"
+
+# Change to base directory
+cd "$base_dir"
+
+# Alignment with Bowtie2 and sorting with Samtools
+bowtie2 -x "$index_dir" -1 "${input_dir}/${sample_id}_unmapped_1.fastq" -2 "${input_dir}/${sample_id}_unmapped_2.fastq" -p 8 | \
+samtools sort -O bam -o "${bam_dir}/${sample_id}_CHM13_sorted.bam"
+
+# Index the sorted BAM file
+samtools index "${bam_dir}/${sample_id}_CHM13_sorted.bam"
+
+# Extract unmapped reads
+samtools view -b -f 4 "${bam_dir}/${sample_id}_CHM13_sorted.bam" > "${bam_dir}/${sample_id}_CHM13_unmapped.bam"
+
+# Convert BAM to FASTQ
+bedtools bamtofastq -i "${bam_dir}/${sample_id}_CHM13_unmapped.bam" \
+-fq "${unmapped_dir}/${sample_id}_final_unmapped_1.fastq" \
+-fq2 "${unmapped_dir}/${sample_id}
