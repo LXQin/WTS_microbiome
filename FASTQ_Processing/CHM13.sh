c7b5327cdf435dbdf2072a45258548e0705775c5
YSPeng <154644354+YSPeng1225@users.noreply.github.com>
2024-08-17 06:15:39 +0800

    Update CHM13.sh

--- FASTQ_Processing/CHM13.sh
+++ FASTQ_Processing/CHM13.sh
@@ -1,13 +1,10 @@
 # Define variables
-base_dir=~/microbiome/CHM13
-index_dir="$base_dir/index/chm13v2.0"
-input_dir=~/microbiome/hg38/unmapped_files
-bam_dir="$base_dir/bam"
-unmapped_dir="$base_dir/unmapped_files"
+base_dir="home/nfs/pengy3/microbiome/CHM13"
+index_dir="home/nfs/pengy3/microbiome/CHM13/index/chm13v2.0"
+input_dir="home/nfs/pengy3/microbiome/hg38/unmapped_files"
+bam_dir="home/nfs/pengy3/microbiome/CHM13/bam"
+unmapped_dir="home/nfs/pengy3/microbiome/CHM13/unmapped_files"
 sample_id="SRR8314071"
-
-# Change to base directory
-cd "$base_dir"
 
 # Alignment with Bowtie2 and sorting with Samtools
 bowtie2 -x "$index_dir" -1 "${input_dir}/${sample_id}_unmapped_1.fastq" -2 "${input_dir}/${sample_id}_unmapped_2.fastq" -p 8 | \
@@ -22,4 +19,4 @@ samtools view -b -f 4 "${bam_dir}/${sample_id}_CHM13_sorted.bam" > "${bam_dir}/$
 # Convert BAM to FASTQ
 bedtools bamtofastq -i "${bam_dir}/${sample_id}_CHM13_unmapped.bam" \
 -fq "${unmapped_dir}/${sample_id}_final_unmapped_1.fastq" \
--fq2 "${unmapped_dir}/${sample_id}
+-fq2 "${unmapped_dir}/${sample_id}_final_unmapped_2.fastq"
